588732926eb5be6bfde70fb64434538a6377dce7
Пожилой молодой человек <49003989+Kladmen228@users.noreply.github.com>
2019-11-11 02:34:46 +0300

    add comments

--- cam.py
+++ cam.py
@@ -7,65 +7,65 @@ from email.mime.text import MIMEText
 import cv2
 import numpy as np
 
-password = "sender password"
-login2 = "sender email"
-url = "smtp.mail.ru"
+password = "sender password"  # пароль почты, с которой производится отправка
+login2 = "sender email"  # логин почты, с которой производится отправка
+url = "smtp.mail.ru"  # mail / yandex / gmail и т.д
 server = smtplib.SMTP_SSL(url, 465)
-target = "target email"
-title = "message title"
-text = "message text"
-filename0 = 'try.jpg'
-filename1 = 'try2.jpg'
+target = "target email"  # целевая почта для отправки
+title = "message title"  # тема сообщения
+text = "message text"  # текст сообщения
+NormalState = 'NormalState.jpg'
+UnNormalState = 'UnNormalState.jpg'
 msg = MIMEMultipart()
 msg['Subject'] = title
-msg['From'] = login2
+msg['From'] = login
 body = text
-acceptable_value_min = 0
-acceptable_value_max = 0
+acceptable_value_min = 0  # минимальное значение погрешности
+acceptable_value_max = 0  # максимальное значение погрешности
 start = True
 
 cam = cv2.VideoCapture(0)
 time.sleep(10)
 
 ret, start_photo = cam.read()
-cv2.imwrite('try.jpg', start_photo)
-cv2.imshow('try.jpg', start_photo)
+cv2.imwrite(NormalState, start_photo)  # сохранение начального состояния
+cv2.imshow(NormalState, start_photo)  # отображение начального состояния
 
 time.sleep(1)
-img1 = cv2.imread(filename0)
+NormalImg = cv2.imread(NormalState)  # начальное состояние
 while True:
     can = True
     ret, frame = cam.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame', gray)
-    cv2.imwrite('try2.jpg', frame)
-    img2 = cv2.imread(filename1)
-    result = cv2.matchTemplate(img1, img2, cv2.TM_CCOEFF_NORMED)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # преобразование в оттенки серого (не используется)
+    cv2.imshow('frame', gray)  # отображение текущего состояния
+    cv2.imwrite(UnNormalState, frame)  # сохранение текущего состояния
+    CurrentImg = cv2.imread(UnNormalState)  # текущее состояние
+    result = cv2.matchTemplate(NormalImg, CurrentImg, cv2.TM_CCOEFF_NORMED)  # сравнение начального состояния с текущим
     y, x = np.unravel_index(result.argmax(), result.shape)
-    if start:
-        acceptable_value_min = result - 0.04
-        acceptable_value_max = result + 0.04
+    if start:  # рассчет погрешности
+        acceptable_value_min = result - 0.04  # мин. погрешность
+        acceptable_value_max = result + 0.04  # макс. погрешность
         start = False
         print(acceptable_value_max, acceptable_value_min)
-    if result < acceptable_value_min or result > acceptable_value_max:
+    if result < acceptable_value_min or result > acceptable_value_max:  # выход за пределы допустимых значений
         print(result)
-        if can:
+        if can:  # отправка сообщения
             can = False
-            fp = open(filename1, 'rb')
-            att = MIMEApplication(fp.read(), _subtype=subtype)
-            fp.close()
-            att.add_header('Content-Disposition', 'attachment', filename=filename1)
+            UnNormalState = open(UnNormalState, 'rb')
+            att = MIMEApplication(UnNormalState.read(), _subtype=subtype)
+            UnNormalState.close()
+            att.add_header('Content-Disposition', 'attachment', filename=UnNormalState)
             msg.attach(att)
             msg.attach(MIMEText(body, 'plain'))
-            # server.login(login2, password)
-            # server.sendmail(login2, target, msg.as_string())
+            server.login(login, password)
+            server.sendmail(login, target, msg.as_string())
             print("take a photo")
     else:
         can = True
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-img1.close()
+NormalImg.close()
 cam.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindows()  # закрытие всех окон
+
